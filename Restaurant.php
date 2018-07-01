7706b85d87478b83f947ceaaeed7266d3f440a89
Pradeep  Gorule <31239579+pradeepgorule@users.noreply.github.com>
2018-07-01 19:01:47 +0530

    Add files via upload

---
+++ Restaurant.php
@@ -0,0 +1,61 @@
+<html>
+<head>
+<title></title>    
+    
+</head>
+    <link rel="stylesheet" href="Restaurant.css">
+       <link rel="stylesheet" href="media.css">
+    <script defer src="https://use.fontawesome.com/releases/v5.0.9/js/all.js" integrity="sha384-8iPTk2s/jMVj81dnzb/iFR2sdA7u06vHJyyLlAd4snFpCl/SnyUjRrbdJsw1pGIl" crossorigin="anonymous"></script>
+<body>
+    <div class="main">
+    <div class="submain">
+        
+        <div class="logo">
+            <img src="St_Regis_Hotels_and_Resorts_logo.png">
+            <div class="navm">
+        <div class="nav">
+            <ul type="none">
+                <a href="#"><li>HOME</li></a>
+                <a href="#"><li>special offers</li></a>
+                <a href="#"><li>Gift Cards</li></a>
+                <a href="#"><li>Contact us</li></a>
+              </ul>    
+        </div>
+                </div>
+            </div>
+          </div>
+            <div class="h">
+                 <h3>YOU GOT 30 MINUTES</h3>
+            
+            </div>
+            <div class="btn">
+                <input type="submit" name="submit" value="order now" id="active">
+                <a link href="menu.html"> <input type="submit" name="submit" value="our menu" ></a>
+            </div>
+        </div>
+  
+    
+    <div class="services">
+        <h1>Our service</h1>
+        <p>Get 2 Regular Pizzas @₹99 each under Everyday Value offer. Available Everyday.
+Buttery Crispy Dough · Exciting Offers · More Toppings · Softer Crust · Hot & Fresh Pizzas
+            Types: Cheese Burst, Garlic Bread, Cheese Dip, Pizza Mania,</p>
+    
+    </div>
+    <div class="ourmenu">
+    </div>
+    <footer>
+        <p>Tweet and share your love with us</p>
+    <h2>Connect with us</h2>
+        <ul>
+        <li><i class="fab fa-facebook"></i></li>
+        <li><i class="fab fa-twitter-square"></i></li>
+        <li><i class="fab fa-linkedin"></i></li>
+        <li><i class="fab fa-google-plus-g"></i></li>
+        <li><i class="fab fa-youtube"></i></li>
+            
+    </ul>
+    </footer>
+    
+    </body>
+</html>
